982119c5cfa274c22212888ab735cd6e7fdc9984
ks-devanshu <ks.devanshu@gmail.com>
2026-06-27 17:59:50 +0530

    Added simplest react query for tvmaze api

--- src/components/ContentView.tsx
+++ src/components/ContentView.tsx
@@ -1,14 +1,22 @@
+import apiClient, { type Query } from "../services/apiClient";
 import HorizontalCardList from "./HorizontalCardList";
 
 
+const query:Query = {
+    genre: 'drama',
+    pageSize: 5
+}
+
 function ContentView() {
+    const {data, error} = apiClient(query);
+
     return (
         <>
         <div className="bg-[#FFFFE3] dark:bg-[#808034]">
-            <HorizontalCardList />
-            <HorizontalCardList />
-            <HorizontalCardList />
-            <HorizontalCardList />
+                <HorizontalCardList shows={data ? data : []} />
+                {/* <HorizontalCardList />
+                <HorizontalCardList />
+                <HorizontalCardList /> */}
         </div>
         </>
     )
--- src/components/HorizontalCardList.tsx
+++ src/components/HorizontalCardList.tsx
@@ -1,18 +1,23 @@
+import type { Show } from "../services/apiClient";
 
 
-const temp:string[] = ['','','','','',''];
+// const temp:string[] = ['','','','','',''];
 
-function HorizontalCardList() {
+interface Props {
+    shows: Show[];
+}
+
+function HorizontalCardList( {shows} : Props ) {
     return (
         <>
-        <h2 className="text-3xl lg:text-6xl py-2 pb-0 lg:py-5 mx-5 mb-3 text-[#723480] dark:text-[#DBD4FF] font-bold">Genres</h2>
+        <h2 className="text-3xl lg:text-6xl py-2 pb-0 lg:py-5 mx-5 mb-0 text-[#723480] dark:text-[#DBD4FF] font-bold">Genres</h2>
         <div className="card card-side max-w-10/10 overflow-scroll overflow-y-hidden scrollbar-none">
-            {temp.map( (each, index) => <figure key={index} className="mx-1 ml-2 shrink-0 w-20 h-30 md:w-30 md:h-40 lg:w-50 lg:h-60 cursor-pointer hover-3d">
+            {shows.map( (each) => <figure key={each.show.id} className="mx-1 ml-2 my-1 shrink-0 w-20 h-30 md:w-30 md:h-40 lg:w-50 lg:h-60 cursor-pointer hover-3d">
                 <img className="rounded-xl"
-                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                alt="Movie" />
+                src={ each.show.image?.original ? each.show.image.original : each.show.image?.medium || "https://static.tvmaze.com/uploads/images/original_untouched/457/1143235.jpg"}
+                alt={each.show.name} />
             </figure> )}
-            <figure className="mx-1 mt-0 my-2 ml-2 shrink-0 w-20 h-30 md:w-30 md:h-40 lg:w-50 lg:h-60 bg-[#DBD4FF] dark:bg-[#FFFFE3] cursor-pointer rounded-xl hover-3d">
+            <figure className="mx-1 mt-1 my-2 ml-2 shrink-0 w-20 h-30 md:w-30 md:h-40 lg:w-50 lg:h-60 bg-[#DBD4FF] dark:bg-[#FFFFE3] cursor-pointer rounded-xl hover-3d">
                 <span className="max-h-fit max-w-fit text-2xl dark:text-[#723480]">➔</span>
             </figure>
         </div>
